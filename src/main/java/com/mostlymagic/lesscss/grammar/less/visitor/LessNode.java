95b2c0e6a0745a79c0fc03fb8a093435e75b1616
Alex Objelean <alex.objelean@gmail.com>
2012-02-29 20:59:57 +0200

    move lesscss project to a separate folder

--- src/main/java/com/mostlymagic/lesscss/grammar/less/visitor/LessNode.java
+++
@@ -1,6 +0,0 @@
-package com.mostlymagic.lesscss.grammar.less.visitor;
-
-
-public interface LessNode {
-	LessVisitorStatus accept(LessVisitor visitor, Context context);
-}
--- src/main/java/com/mostlymagic/lesscss/parser/LessStyleSheetParser.java
+++
@@ -1,5 +0,0 @@
-package com.mostlymagic.lesscss.parser;
-
-public class LessStyleSheetParser{
-
-}
